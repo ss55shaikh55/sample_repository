19e2143655aee4d5240c8824c17964487f40b5a3
ss55shaikh55 <ss55.shaikh55@gmail.com>
2022-10-24 20:50:22 +0530

    First Commit

---
+++ GitPractice.java
@@ -0,0 +1,7 @@
+class GitPractice{
+	public static void mainI(String []args){
+		System.out.print("This is my 1st git practive program");
+		System.out.print("This is my 2nd git practive program");
+		S
+	}
+}
